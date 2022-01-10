3098d44ad01454d5fd717cbdd0371f61eaac29e3
Son Nguyen <sonnn.uct@gmail.com>
2022-01-10 12:34:16 +0700

    init

---
+++ mail.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"net/smtp"
+	"regexp"
+	"strings"
+	"time"
+)
+
+const TimeOutDial = time.Second * 5
+func ValidRegex(email string) error {
+	var emailRegexp = regexp.MustCompile(`(?m)^(((((((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?([A-Za-z0-9!#-'*+\/=?^_\x60{|}~-])+((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?)|(((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?"((\s? +)?(([!#-[\]-~])|(\\([ -~]|\s))))*(\s? +)?"))?)?(((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?<(((((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?(([A-Za-z0-9!#-'*+\/=?^_\x60{|}~-])+(\.([A-Za-z0-9!#-'*+\/=?^_\x60{|}~-])+)*)((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?)|(((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?"((\s? +)?(([!#-[\]-~])|(\\([ -~]|\s))))*(\s? +)?"))@((((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?(([A-Za-z0-9!#-'*+\/=?^_\x60{|}~-])+(\.([A-Za-z0-9!#-'*+\/=?^_\x60{|}~-])+)*)((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?)|(((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?\[((\s? +)?([!-Z^-~]))*(\s? +)?\]((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?)))>((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?))|(((((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?(([A-Za-z0-9!#-'*+\/=?^_\x60{|}~-])+(\.([A-Za-z0-9!#-'*+\/=?^_\x60{|}~-])+)*)((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?)|(((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?"((\s? +)?(([!#-[\]-~])|(\\([ -~]|\s))))*(\s? +)?"))@((((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?(([A-Za-z0-9!#-'*+\/=?^_\x60{|}~-])+(\.([A-Za-z0-9!#-'*+\/=?^_\x60{|}~-])+)*)((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?)|(((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?\[((\s? +)?([!-Z^-~]))*(\s? +)?\]((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?))))$`)
+	if !emailRegexp.MatchString(strings.ToLower(email)) {
+		return errors.New("Invalid_Email")
+	}
+	return nil
+}
+
+func CheckRCPT(serverHostName, serverMailAddress, email string) error {
+	_, host := split(email)
+	mx, err := net.LookupMX(host)
+	if err != nil {
+		return err
+	}
+	client, err := DialTimeout(fmt.Sprintf("%s:%d", mx[0].Host, 25), TimeOutDial)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	err = client.Hello(serverHostName)
+	if err != nil {
+		return err
+	}
+	err = client.Mail(serverMailAddress)
+	if err != nil {
+		return err
+	}
+	err = client.Rcpt(email)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DialTimeout(addr string, timeout time.Duration) (*smtp.Client, error) {
+	conn, err := net.DialTimeout("tcp", addr, timeout)
+	if err != nil {
+		return nil, err
+	}
+
+	t := time.AfterFunc(timeout, func() { conn.Close() })
+	defer t.Stop()
+
+	host, _, _ := net.SplitHostPort(addr)
+	return smtp.NewClient(conn, host)
+}
+
+func split(email string) (account, host string) {
+	i := strings.LastIndexByte(email, '@')
+	// If no @ present, not a valid email.
+	if i < 0 {
+		return
+	}
+	account = email[:i]
+	host = email[i+1:]
+	return
+}
+
+func PrintErrorResponse(err error)  {
+	fmt.Printf("Error Code: %v, Message: \"%v\"", err.Error()[0:3], err)
+}
+
+func main() {
+	RETRY:
+	fmt.Print("Enter email: ")
+	var email string
+	_, _ = fmt.Scanln(&email)
+	err := ValidRegex(email)
+	if err != nil {
+		PrintErrorResponse(err)
+	}
+
+	var (
+		testHost    = "smtp.vccorp.vn"
+		testMail = "sonnguyennam@vccorp.vn"
+	)
+
+	fmt.Printf("\nRcpt by: %v --> %v\n", testMail, email)
+	err = CheckRCPT(testHost, testMail, email)
+	if err != nil {
+		PrintErrorResponse(err)
+	}
+
+	if err == nil {
+		fmt.Println("\n---> Email is valid", "")
+	}
+
+	fmt.Println("\nTry again? press Enter \"x\" to exit...")
+	var retry string
+	_, _ = fmt.Scanln(&retry)
+	if retry != "x" {
+		goto RETRY
+	}
+	fmt.Println("Exiting...")
+	time.Sleep(2*time.Second)
+	return
+}
